7cfc0654ef438aae236465cee5a82f120f0471d2
Steve-omush <steveprogram60@gmail.com>
2023-11-16 12:14:44 -0800

    Task 0 and shell executable hsh

--- shell.c
+++ shell.c
@@ -1,94 +1,158 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
 #include <dirent.h>
+#include <signal.h>
 
-#define MAX_COMMAND_LENGTH 1024
+#define MAX_INPUT_SIZE 1024
 
-void display_prompt(void);
-void execute_command(char *command);
+int is_terminal(int fd);
+void execute_command(char *command, char *argv[]);
 
 /**
- * display_prompt - display the shell.
+ * is_terminal - checks if a fd corresponds to terminal.
+ * @fd: file descriptor.
  *
- * Return: Nothing.
+ * Return: 1 if file, otherwise 0.
  */
-void display_prompt(void)
+int is_terminal(int fd)
 {
-	printf("#cisfun$ ");
-	fflush(stdout);
+	struct stat buf;
+
+	if (fstat(fd, &buf) == -1)
+	{
+		perror("fstat");
+		exit(EXIT_FAILURE);
+	}
+	return (S_ISCHR(buf.st_mode));
 }
 
 /**
- * execute_command - executes commands in shell.
- * @command: executable command.
+ * execute_command - executes commands.
+ * @command: command entered in the shell.
+ * @argv: executable file name of shell.
  *
  * Return: Nothing.
  */
-void execute_command(char *command)
+void execute_command(char *command, char *argv[])
 {
 	pid_t pid;
 	int status;
-
-	char *args[2];
-	args[0] = command;
-	args[1] = NULL;
+	char *path;
+	char *path_token;
 
 	pid = fork();
+
 	if (pid == 0)
 	{
-		execve(args[0], args, NULL);
-		dprintf(STDERR_FILENO, "./shell: No such file or directory\n");
+		char *token = strtok(command, " ");
+		char *args[MAX_INPUT_SIZE];
+
+		int i = 0;
+
+		while (token != NULL)
+		{
+			args[i++] = token;
+			token = strtok(NULL, " ");
+		}
+		args[i] = NULL;
+
+		path = getenv("PATH");
+		path_token = strtok(path, ":");
+
+		if (i > 1)
+		{
+			fprintf(stderr, "%s: No such file or directory\n", argv[0]);
+			_exit(EXIT_FAILURE);
+		}
+		while (path_token != NULL)
+		{
+			if (access(args[0], X_OK) == 0)
+			{
+				execve(args[0], args, NULL);
+				fprintf(stderr, "%s: No such file or directory\n", argv[0]);
+				_exit(EXIT_FAILURE);
+			}
+			path_token = strtok(NULL, ":");
+		}
+		fprintf(stderr, "%s: No such file or directory\n", argv[0]);
 		_exit(EXIT_FAILURE);
 	}
-	else if (pid > 0)
-	{
-		waitpid(pid, &status, 0);
-	}
+	else if (pid < 0)
+		perror("fork");
 	else
 	{
-		dprintf(STDERR_FILENO, "./shell: No such file or directory\n");
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
 }
 
 /**
- * main: main function.
+ * main - main function.
+ * @argc: argument counter.
+ * @argv: argument vector.
  *
- * Return: 0 Success.
+ * Return: O on success.
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-	char *command = NULL;
-	size_t len = 0;
-	ssize_t read;
+	char *input;
+	size_t input_size, len;
+	pid_t wpid;
+	int status;
 
-	while (1)
+	(void)argc;
+	if (is_terminal(STDIN_FILENO))
 	{
-		display_prompt();
-
-		if ((read = getline(&command, &len, stdin)) == -1)
+		while (1)
 		{
-			free(command);
-			exit(EXIT_SUCCESS);
-		}
+			printf("($) ");
+			if (getline(&input, &input_size, stdin) == -1)
+			{
+				perror("getline");
+				exit(EXIT_FAILURE);
+			}
+			len = strlen(input);
+			if (len > 0 && input[len - 1] == '\n')
+				input[len - 1] = '\0';
 
-		if (read > 0 && command[read - 1] == '\n')
-		{
-			command[read - 1] = '\0';
-		}
+			if (strcmp(input, "exit") == 0)
+			{
+				fprintf(stderr, "%s: No such file or directory\n", argv[0]);
+				continue;
+			}
+			execute_command(input, argv);
+			fflush(stdin);
+			clearerr(stdin);
 
-		if (strcmp(command, "exit") == 0)
-		{
-			free(command);
-			exit(EXIT_SUCCESS);
+			do
+				wpid = waitpid(-1, &status, WNOHANG);
+			while (wpid > 0);
 		}
-
-		execute_command(command);
 	}
+	else
+	{
+		while (getline(&input, &input_size, stdin) != -1)
+		{
+			len = strlen(input);
+			if (len > 0 && input[len - 1] == '\n')
+				input[len - 1] = '\0';
+
+			execute_command(input, argv);
+			fflush(stdin);
+			clearerr(stdin);
+
+			do
+				wpid = waitpid(-1, &status, WNOHANG);
+			while (wpid > 0);
+		}
+	}
+	free(input);
 	return (0);
-}	
+}
